6ab8af1edc264ac87051e41541622cb6e9db8dae
Allan Pereira <pereirasallan@gmail.com>
2024-04-24 03:26:45 -0300

    Add URL config with Dashboard view in Bills app

---
+++ bills/urls.py
@@ -0,0 +1,9 @@
+"""
+URL configuration for app bills.
+"""
+from django.urls import path
+from bills import views
+
+urlpatterns = [
+    path('', views.DashboardView.as_view(), name="dashboard")
+]
