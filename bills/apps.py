262c6e84d4754f94cc40a2e0560021a9b68be097
Allan Pereira <pereirasallan@gmail.com>
2024-04-25 05:38:36 -0300

    Update Bills App config

--- bills/apps.py
+++ bills/apps.py
@@ -8,13 +8,7 @@ class BillsConfig(AppConfig):
     This application provides functionalities related to visualizing all of
     the bills that legislators voted for or against.
     """
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bills'
-    verbose_name = 'Legislature bills voting'
 
-    def ready(self):
-        """
-        This method is called when the bills app is ready. Use this method to
-        connect signals or perform other app-specific startup tasks.
-        """
-        # import bills.signals  # Import and potentially connect signals here
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "bills"
+    verbose_name = "Legislature bills voting"
