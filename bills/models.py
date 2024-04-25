60d44f092790f5c76384f058c2d1f5449c7f2999
Allan Pereira <pereirasallan@gmail.com>
2024-04-25 05:40:00 -0300

    Adjusted Models constraints

--- bills/models.py
+++ bills/models.py
@@ -5,6 +5,7 @@ class Legislator(models.Model):
     """
     Model representing a Person, an individual elected to government.
     """
+
     name = models.CharField(max_length=255)
 
     def __str__(self) -> str:
@@ -15,8 +16,11 @@ class Bill(models.Model):
     """
     Model representing a piece of legislation introduced in the Congress.
     """
+
     title = models.CharField(max_length=255)
-    sponsor = models.ForeignKey(Legislator, on_delete=models.PROTECT)
+    sponsor = models.ForeignKey(
+        Legislator, on_delete=models.PROTECT, related_name="sponsored_bills"
+    )
 
     def __str__(self) -> str:
         return f"{self.title}"
@@ -26,7 +30,8 @@ class Vote(models.Model):
     """
     Model representing a vote on a particular Bill.
     """
-    bill = models.ForeignKey(Bill, on_delete=models.CASCADE)
+
+    bill = models.ForeignKey(Bill, related_name="votes", on_delete=models.CASCADE)
 
 
 class VoteResult(models.Model):
@@ -39,9 +44,19 @@ class VoteResult(models.Model):
         """
         Represents the available types of Votes.
         """
-        YAE = 1
+
+        YEA = 1
         NAY = 2
 
-    legislator = models.ForeignKey(Legislator, on_delete=models.PROTECT)
-    vote = models.ForeignKey(Vote, on_delete=models.CASCADE)
+    legislator = models.ForeignKey(
+        Legislator, related_name="voting_results", on_delete=models.PROTECT
+    )
+    vote = models.ForeignKey(Vote, related_name="results", on_delete=models.CASCADE)
     vote_type = models.IntegerField(choices=VoteType.choices)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                "legislator", "vote", name="unique_legislator_vote_per_bill"
+            )
+        ]
